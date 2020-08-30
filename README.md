b77ce4ecd1c6de1ef4d5647869107b7f114dd35e
mzuenni <michi.zuendorf@gmail.com>
2020-08-31 00:40:12 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # domjudge-testlib-wrapper
-A wrapper for the testlib.h to work with the Domjudge
+A wrapper for the [testlib.h](https://github.com/MikeMirzayanov/testlib) to work with the [DOMjudge](https://www.domjudge.org/).
+You only need to include ``domjudge.h`` instead of ``testlib.h``.
+This header will redefine the exitcodes to ``42`` for AC submissions and ``43`` for WA submissin, as expected by DOMjudge.
+Additionally it changes the functions ``registerTestlibCmd`` and ``registerInteraction`` to handle the parameters in ``argv`` correct.
---
+++ domjudge.h
@@ -0,0 +1,62 @@
+// define exitcodes for testlib.h
+#ifdef OK_EXIT_CODE
+#   undef OK_EXIT_CODE
+#endif
+#define OK_EXIT_CODE 42
+
+#ifdef WA_EXIT_CODE
+#   undef WA_EXIT_CODE
+#endif
+#define WA_EXIT_CODE 43
+
+#ifdef PE_EXIT_CODE
+#   undef PE_EXIT_CODE
+#endif
+#define PE_EXIT_CODE 43
+
+#ifdef DIRT_EXIT_CODE
+#   undef DIRT_EXIT_CODE
+#endif
+#define DIRT_EXIT_CODE 43
+
+#ifdef UNEXPECTED_EOF_EXIT_CODE
+#   undef UNEXPECTED_EOF_EXIT_CODE
+#endif
+#define UNEXPECTED_EOF_EXIT_CODE 43
+
+#define registerTestlibCmd _domjudge_registerTestlibCmd
+#define registerInteraction _domjudge_registerInteraction
+#include "testlib.h"
+#undef registerInteraction
+#undef registerTestlibCmd
+
+struct ArgConvert {
+	std::string binary, testin, testout, feedback;
+	std::vector<char*> testlibArgs;
+	ArgConvert(int argc, char* argv[]) {
+		if (argc != 4) quit(_fail, "wrong number of parameters!");
+		binary = std::string(argv[0]);
+		testin = std::string(argv[1]);
+		testout = std::string(argv[2]);
+		feedback = std::string(argv[3]);
+		feedback += "/judgemessage.txt";
+
+		testlibArgs.push_back(&binary[0]);
+		testlibArgs.push_back(&testin[0]);
+		testlibArgs.push_back(&feedback[0]);
+		testlibArgs.push_back(&testout[0]);
+		testlibArgs.push_back(nullptr);
+	}
+	int argc() {return 4;}
+	char** argv() {return testlibArgs.data();}
+};
+
+void registerTestlibCmd(int argc, char* argv[]) {
+	ArgConvert tmp(argc, argv);
+	_domjudge_registerTestlibCmd(tmp.argc(), tmp.argv());
+}
+
+void registerInteraction(int argc, char* argv[]) {
+	ArgConvert tmp(argc, argv);
+	_domjudge_registerInteraction(tmp.argc(), tmp.argv());
+}
